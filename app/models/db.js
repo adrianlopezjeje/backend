5d2052b0c7100d9febb8c3dea2680e808ebea719
fjsuarez <fjsuarez@uniovi.es>
2024-01-16 16:46:16 +0100

    cambios 2024

---
+++ app/models/db.js
@@ -0,0 +1,30 @@
+// Database creation 
+
+const dbConfig = require('../config/db.config.js');
+const Sequelize = require('sequelize');
+
+// Create a connection pool to the database
+const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
+    host: dbConfig.HOST,
+    port: dbConfig.PORT,
+    dialect: dbConfig.dialect,
+    operatorAliases: false,
+    logging: false,
+    pool: {
+        max: dbConfig.max,
+        min: dbConfig.min,
+        idle: dbConfig.idle,
+        acquire: dbConfig.acquire
+    }
+});
+
+const db = {};
+
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+// Load models
+db.Book = require('./book.model.js')(sequelize, Sequelize);
+db.User = require('./user.model.js')(sequelize, Sequelize);
+
+module.exports = db;
